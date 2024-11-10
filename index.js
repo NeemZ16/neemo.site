51263814b4e0b5b2aa87e572dfe24c60dae8f262
NeemZ16 <110858265+NeemZ16@users.noreply.github.com>
2024-11-10 00:08:29 -0500

    added input unstyled

--- index.js
+++ index.js
@@ -6,19 +6,16 @@ displayPrompt();
 
 
 function displayBanner() {
-  // create ascii art element
   const asciiArt = document.createElement("pre");
   asciiArt.innerHTML = `
    ____  ___  ___  ____ ___  ____     ____
-  / __ \\/ _ \\/ _ \\/ __ __  \\/ __ \\   /_  /
+  / __ \\/ _ \\/ _ \\/ __ \`__ \\/ __ \\   /_  /
  / / / /  __/  __/ / / / / / /_/ /    / /_
 /_/ /_/\\___/\\___/_/ /_/ /_/\\____/    /___/  v1.0.0\n\n\n`;
 
-  // create help text element
   const helpText = document.createElement("p")
-  helpText.textContent = "Type 'help' for list of supported commands"
+  helpText.textContent = "Type 'help' for list of supported commands";
 
-  // add elements
   terminal.appendChild(asciiArt);
   asciiArt.insertAdjacentElement("afterend", helpText);
 }
@@ -28,17 +25,25 @@ function displayPrompt() {
   const domain = document.createElement("span");
   domain.innerHTML = "@" + window.location.hostname;
   domain.classList.add("domain");
+  
   const user = document.createElement("span");
   user.classList.add("user");
   user.innerHTML = "guest";
+  
   const directory = document.createElement("span");
   directory.classList.add("directory");
   directory.innerHTML = ":" + dir + " $&nbsp;";
 
+  // create textarea
+  const input = document.createElement("input");
+
+
   // create prompt
-  const prompt = document.createElement("prompt")
+  const prompt = document.createElement("div");
+  prompt.classList.add("prompt");
   prompt.appendChild(user);
   prompt.appendChild(domain);
   prompt.appendChild(directory);
+  prompt.appendChild(input);
   terminal.appendChild(prompt);
 }
